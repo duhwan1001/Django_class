8e6688e45a68dfd127bac3e3bf88b34aed32b20b
duhwan1001 <kdhz2012@gmail.com>
2022-12-06 19:32:23 +0900

    firstCommit

---
+++ day1(rocket)/ex01.py
@@ -0,0 +1,7 @@
+class Maple:
+    힘 = 10
+
+def add():
+    return 20
+
+B = 30
---
+++ day1(rocket)/ex02.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.path)
+
+import random
+print(random.randint(1,10))
+
+import ex01
+
+A = ex01.Maple()
+B = ex01.add()
+C = ex01.B
+
+print(A.힘, B, C)
